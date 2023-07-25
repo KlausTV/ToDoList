f70ae9310401bd03e0bfbdcd982955809e546bdc
KlasterTV <elasticgaming10@gmail.com>
2023-07-24 22:01:58 -0400

    Fixed the background image it now works

--- project-5.js
+++ project-5.js
@@ -1,13 +1,19 @@
+// Variables
+
 const addTask = document.getElementById("add-task");
 const taskContainer = document.getElementById("task-container");
 const inputTask = document.getElementById("input-task");
 
+// Event Listener for Add Button
+
 addTask.addEventListener("click", function () {
   let task = document.createElement("div");
   task.classList.add("task");
+
   let li = document.createElement("li");
   li.innerText = `${inputTask.value}`;
   task.appendChild(li);
+
   let checkButton = document.createElement("button");
   checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
   checkButton.classList.add("checkTask");
@@ -19,21 +25,20 @@ addTask.addEventListener("click", function () {
   task.appendChild(deleteButton);
 
   if (inputTask.value === "") {
-    alert("Please enter a task");
+    alert("Please Enter a Task");
   } else {
     taskContainer.appendChild(task);
   }
+
   inputTask.value = "";
 
-  // Mark task as done (line-through)
   checkButton.addEventListener("click", function () {
-    li.style.textDecoration = "line-through";
+    checkButton.parentElement.style.textDecoration = "line-through";
   });
 
-  // Delete a task
   deleteButton.addEventListener("click", function (e) {
     let target = e.target;
+
     target.parentElement.parentElement.remove();
   });
 });
-d;
